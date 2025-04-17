fa0f13b314f27219ca9c01806385f5d6090562be
IDHRES AHMED MI <48350502+idreescg@users.noreply.github.com>
2025-04-17 10:33:58 +0530

    Update helm.sh

--- helm.sh
+++ helm.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+# Download the Helm installation script
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-chmod 700 get_helm.sh
-sudo sh get_helm.sh
+
+# Check if the download was successful
+if [ -f get_helm.sh ]; then
+    echo "Download successful."
+    # Make the script executable
+    chmod 700 get_helm.sh
+    # Run the script with superuser privileges
+    sudo sh get_helm.sh
+else
+    echo "Download failed. Please check the URL."
+fi
